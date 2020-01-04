a6221f2439925b31d2820f3448f1ecccbf15c83f
juswill <juswill.yang@100tal.com>
2020-01-04 16:16:39 +0800

    初始化

---
+++ Xcode11.3Test/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Xcode11.3Test
+//
+//  Created by 杨静伟 on 2020/1/4.
+//  Copyright © 2020 firstleap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+      
+    }
+}
